12af40c36a19213134fb1fd52991036814c11dd0
Gouse Basha <gousebashaskk@gmail.com>
2025-12-15 19:39:46 +0530

    swap program in java

---
+++ swaping.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+public class swaping {
+    public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the number ");
+        
+   /*  int a = 5;  
+    int b = 10;   // Swap without 3rd variable
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    System.out.println("a = "+ a);
+    System.out.println("b = "+ b);*/
+
+    int a = 20, b=25;
+      int temp;
+
+      temp = a;
+      a = b;
+      b = temp;
+      System.out.println("a=" + a);
+      System.out.println("b= " + b);
+
+
+    }
+    
+}
